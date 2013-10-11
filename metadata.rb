0791c9d878da51d840ee3336b16419187982469e
rackerjoe <breu@breu.org>
2013-10-11 14:36:32 -0500

    move version out of metadata.rb into VERSION

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Rackspace Hosting, Inc."
 license           "Apache 2.0"
 description       "tempest module"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "4.1.3"
+version           IO.read(File.join(File.dirname(__FILE__), "VERSION"))
 
 %w{ ubuntu }.each do |os|
   supports os
