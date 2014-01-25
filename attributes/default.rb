59c8ad115a4057e21c5c99470fb816929c11bdc4
Hugh Saunders <hugh@wherenow.org>
2014-01-25 09:57:11 +0000

    working on it

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,25 +1,21 @@
-default["tempest"]["branch"] = nil                           # node_attribute
-default["tempest"]["use_ssl"] = false                        # node_attribute
-default["tempest"]["tenant_isolation"] = true                # node_attribute
-default["tempest"]["tenant_reuse"] = true                    # node_attribute
-default["tempest"]["alt_ssh_user"] = "cirros"                # node_attribute
-default["tempest"]["ssh_user"] = "cirros"                    # node_attribute
-default["tempest"]["user1"] = "tempest_monitor_user1"        # node_attribute
-default["tempest"]["user1_pass"] = "monpass"                 # node_attribute
-default["tempest"]["user1_tenant"] = "tempest_monitor_tenant1" # node_attribute
-default["tempest"]["user2"] = "tempest_monitor_user2"        # node_attribute
-default["tempest"]["user2_pass"] = "monpass"                 # node_attribute
-default["tempest"]["user2_tenant"] = "tempest_monitor_tenant2" # node_attribute
-default["tempest"]["test_img1"]["id"] = nil                        # node_attribute
-default["tempest"]["test_img1"]["url"] = "https://launchpadlibrarian.net/83305348/cirros-0.3.0-x86_64-disk.img"
-default["tempest"]["test_img2"] = "12123"                    # node_attribute
-default["tempest"]["img1_flavor"] = "1"                      # node_attribute
-default["tempest"]["img2_flavor"] = "2"                      # node_attribute
-default["tempest"]["admin"] = "admin"                        # node_attribute
-default["tempest"]["admin_pass"] = "secrete"                 # node_attribute
-default["tempest"]["admin_tenant"] = "admin"                 # node_attribute
-default["tempest"]["runlist"] = ["tempest.tests.compute.images.test_images_oneserver",
-                                 "tempest.tests.image.v2.test_images"]
-# how often the tests are run. This is in minutes
-default["tempest"]["interval"] = "5"                         # node_attribute
-default["tempest"]["use_cron"] = false                       # node_attribute
+# encoding: UTF-8
+default['openstack']['tempest']['branch'] = nil
+default['openstack']['tempest']['disable_ssl_validation'] = false
+default['openstack']['tempest']['tenant_isolation'] = true
+default['openstack']['tempest']['tenant_reuse'] = true
+default['openstack']['tempest']['alt_ssh_user'] = 'cirros'
+default['openstack']['tempest']['ssh_user'] = 'cirros'
+default['openstack']['tempest']['user1'] = 'tempest_user1'
+default['openstack']['tempest']['user1_pass'] = 'tempest_user1_pass'
+default['openstack']['tempest']['user1_tenant'] = 'tempest_tenant1'
+default['openstack']['tempest']['user2'] = 'tempest_user2'
+default['openstack']['tempest']['user2_pass'] = 'tempest_user2_pass'
+default['openstack']['tempest']['user2_tenant'] = 'tempest_tenant2'
+default['openstack']['tempest']['test_img1']['id'] = nil
+default['openstack']['tempest']['test_img1']['url'] = 'https://launchpadlibrarian.net/83305348/cirros-0.3.0-x86_64-disk.img'
+default['openstack']['tempest']['test_img2'] = '12123'
+default['openstack']['tempest']['img1_flavor'] = '1'
+default['openstack']['tempest']['img2_flavor'] = '2'
+default['openstack']['tempest']['admin'] = 'admin'
+default['openstack']['tempest']['admin_pass'] = 'secrete'
+default['openstack']['tempest']['admin_tenant'] = 'admin'
